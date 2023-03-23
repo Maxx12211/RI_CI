8996b7bf9c80c377aeb9ee0c3d3084eab97502a1
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2023-03-23 16:41:40 +0530

    Recude the -j  value

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-bliss device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
@@ -15,7 +15,7 @@ mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-blissify rova -j16  > reading & sleep 95m
+blissify rova -j8  > reading & sleep 95m
 
 retVal=$?
 timeEnd
