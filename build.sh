63220bb2a8a980936e85e27785ee078db553d47c
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2023-03-24 17:26:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13-spark device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
@@ -11,6 +11,6 @@ chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
 ./barom.sh --device rova --lunch spark_rova-userdebug
-./barom.sh -b -j 8 -u wet --timer 95m -- mka bacon
+./barom.sh -b -j 10 -u wet --timer 95m -- mka bacon
 
 # end 
