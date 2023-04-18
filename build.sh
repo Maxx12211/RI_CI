0ee8a45f5380f7f05db43004e5b3286b7eafc2b2
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2023-04-18 15:27:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-#sudo apt-get install git-lfs
-#repo init --git-lfs
-#rm -rf external/chromium-webview/prebuilt/*
-#rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-#rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-#repo sync -j16
 git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-blaze device/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
 git clone --depth=1 https://github.com/Maxx12211/kernel_xiaomi_rova.git -b a13 kernel/xiaomi/rova
@@ -17,7 +10,7 @@ source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch blaze_rova-eng
+lunch blaze_rova-userdebug
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
