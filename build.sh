1d582294db03b8de4b3a499022a0b36e0e662e77
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2023-04-23 21:00:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13.0-alphadroid device/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13.0 vendor/xiaomi/rova
-git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/device_xiaomi_rova.git -b a13-risingos device/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/vendor_xiaomi_rova.git -b a13 vendor/xiaomi/rova
+git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_msm8937.git -b a13/master kernel/xiaomi/msm8937
+#curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+#sudo apt-get install git-lfs
+#repo init --git-lfs
+#rm -rf external/chromium-webview/prebuilt/*
+#rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+#rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+#git clone --depth=1 https://github.com/Maxx12211/android_device_xiaomi_rova.git -b 13.0-alphadroid device/xiaomi/rova
+#git clone --depth=1 https://github.com/Maxx12211/android_vendor_xiaomi_rova.git -b 13.0 vendor/xiaomi/rova
+#git clone --depth=1 https://github.com/Maxx12211/android_kernel_xiaomi_rova.git -b 13.0 kernel/xiaomi/rova
 
 # build rom
+mv hardware/lineage/compat/Android.bp hardware/lineage/compat/Android.bpp
 curl -Lo barom.sh https://raw.githubusercontent.com/alanndz/barom/main/barom.sh
 chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
