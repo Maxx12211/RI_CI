810321249e2f71fc1159ee4149fa1eb2283bf060
Maxx1221 <99007837+Maxx12211@users.noreply.github.com>
2023-04-24 21:46:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka bacon -j8  > reading & sleep 90m
+mka bacon -j8  > reading & sleep 80m
 
 retVal=$?
 timeEnd
